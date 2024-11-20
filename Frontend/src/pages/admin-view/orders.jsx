3572cc25808b4dd71072f9f5429ffba80d1e4eab
Shivasai Mamidala <105928178+shivasai789@users.noreply.github.com>
2024-11-20 17:18:05 +0530

    admin orders 2

--- Frontend/src/pages/admin-view/orders.jsx
+++ Frontend/src/pages/admin-view/orders.jsx
@@ -1,13 +1,16 @@
 import AdminOrdersView from "@/components/admin-view/orders";
 import { TabsContent } from "@/components/ui/tabs";
+import { Tabs } from "@radix-ui/react-tabs";
 
 
 function AdminOrders() {
     return ( 
         <div>
+            <Tabs>
             <TabsContent>
             <AdminOrdersView/>
             </TabsContent>
+            </Tabs>
         </div>
      );
 }
